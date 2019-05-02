04d9510f67a6cd381947015921d95fe51915e883
maxim <fleksso99@gmail.com>
2019-05-02 15:14:40 -0700

    update1

--- target/classes/data.sql
+++ target/classes/data.sql
@@ -1,3 +1,5 @@
 INSERT INTO USER VALUES(10001, sysdate(), 'AB');
 INSERT INTO USER VALUES(10002, sysdate(), 'Jill');
 INSERT INTO USER VALUES(10003, sysdate(), 'Jam');
+insert into post values(11001, 'My first post', 10001);
+insert into post values(11002, 'My second post', 10001);
